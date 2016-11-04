38c79f3d71c11838bba361dbdf7753f3aace97c3
Matheus martins <matheusmr00@gmail.com>
2016-11-04 15:57:42 -0200

    Removing some configs

--- frontend/src/js/config/Config.js
+++ frontend/src/js/config/Config.js
@@ -1,8 +1,4 @@
 var Config = {};
 Config.basePageUrl = '/pages';
-Config.baseBundleUrl = '/bundle';
 Config.apiUrl = '/api';
-Config.noauthUrl = '/noauth';
-Config.expirationDelta = 1000 * 60 * 60 * 24;
-Config.timeToHideMessage = 8000;
 module.exports = Config;
